933437e8c37b786eb9eb0b5e3d6b4afe42e88686
Mohammad Yusuf <yusufmohammad11533@gmail.com>
2026-06-28 10:19:16 +0530

    Connect frontend to Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://todo-backend-6fei.onrender.com/api",
 });
 
 export default api;
