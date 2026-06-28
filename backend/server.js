82a113d818d69c1df9f7d292b1cd7f0f4ac1c9fa
Mohammad Yusuf <yusufmohammad11533@gmail.com>
2026-06-28 09:45:49 +0530

    Fix Render port

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const { readTodos, writeTodos } = require("./db");
 const { v4: uuidv4 } = require("uuid");
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
